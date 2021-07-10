79203464e9a6927b505a2698c0b7f33f01800714
nekoko2874 <meshitchi@gmail.com>
2021-07-11 00:50:40 +0900

    header部分の作成したもの。

--- collectingDiscountInformationProjectFront/src/app/app.module.ts
+++ collectingDiscountInformationProjectFront/src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';  // <-- 追加
+import { MatToolbarModule } from '@angular/material/toolbar';  // <-- 追加
+import { MatSidenavModule } from '@angular/material/sidenav';  // <-- 追加
+import { MatListModule } from '@angular/material/list';  // <-- 追加
+import { HeaderComponent } from './header/header.component';
+
+
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,  // <-- 追加
+    MatToolbarModule,  // <-- 追加
+    MatSidenavModule,  // <-- 追加
+    MatListModule,  // <-- 追加
+
+    
   ],
-  providers: [],
+  
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
