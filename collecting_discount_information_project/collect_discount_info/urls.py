80be1201c4f7257f427a3d5d2431b127d742d354
Shiro-464646 <sHiro57723@gmail.com>
2021-07-31 22:38:25 +0900

    ユーザーの権限ごとに表示する画面を変更。ログインしていないとログインしてください画面になる。

--- collecting_discount_information_project/collect_discount_info/urls.py
+++ collecting_discount_information_project/collect_discount_info/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.profile, name='profile'),
 ]
--- collecting_discount_information_project/collect_discount_info/views.py
+++ collecting_discount_information_project/collect_discount_info/views.py
@@ -5,4 +5,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello Login. You're at the collect_discount_info index.")
+    return HttpResponse("Hello Login. BOOOOOOOOO.")
+
+def profile(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return HttpResponse("You are a superuser.")
+            #あとで使う
+            #return redirect(HttpResponse("Hello Login. You're at the collect_discount_info index."))
+        else:
+            return HttpResponse("Hello Login. You're at the collect_discount_info index.")
+    else:
+        return HttpResponse("You can't use it. Please log in.")
